26df606a9f80c06fa0f5ba6093a6ca2c4ea77887
Hades <Hades@github.com>
2018-04-17 17:33:28 +0600

    Android permission updated

--- app/src/main/java/bd/com/mcc/www/hadesmusicplayer/MainActivity.java
+++ app/src/main/java/bd/com/mcc/www/hadesmusicplayer/MainActivity.java
@@ -54,28 +54,28 @@ public class MainActivity extends Activity implements MediaPlayerControl {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= 23)
-        {
-                // Code for above or equal 23 API Oriented Device
-                // Your Permission granted already .Do next code
-                requestPermission();
+        if (Build.VERSION.SDK_INT >= 23) {
+            // Code for above or equal 23 API Oriented Device
+            // Your Permission granted already .Do next code
+            requestPermission();
+
+        } else {
+            ListView songView = (ListView) findViewById(R.id.song_list);
+            songList = new ArrayList<Song>();
+            getSongList();
+
+            Collections.sort(songList, new Comparator<Song>() {
+                public int compare(Song a, Song b) {
+                    return a.getTitle().compareTo(b.getTitle());
+                }
+            });
+
+
+            SongAdapter songAdt = new SongAdapter(this, songList);
+            songView.setAdapter(songAdt);
+
+            setController();
         }
-
-        ListView songView = (ListView) findViewById(R.id.song_list);
-        songList = new ArrayList<Song>();
-        getSongList();
-
-        Collections.sort(songList, new Comparator<Song>(){
-            public int compare(Song a, Song b){
-                return a.getTitle().compareTo(b.getTitle());
-            }
-        });
-
-
-        SongAdapter songAdt = new SongAdapter(this, songList);
-        songView.setAdapter(songAdt);
-
-        setController();
     }
 
     //connect to the service
@@ -300,12 +300,26 @@ public class MainActivity extends Activity implements MediaPlayerControl {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
         }
     }
-    @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                    ListView songView = (ListView) findViewById(R.id.song_list);
+                    songList = new ArrayList<Song>();
+                    getSongList();
+
+                    Collections.sort(songList, new Comparator<Song>() {
+                        public int compare(Song a, Song b) {
+                            return a.getTitle().compareTo(b.getTitle());
+                        }
+                    });
+
+
+                    SongAdapter songAdt = new SongAdapter(this, songList);
+                    songView.setAdapter(songAdt);
+
+                    setController();
+                    Log.e("value", "Permission     @Override\nGranted, Now you can use local drive .");
                 } else {
                     Log.e("value", "Permission Denied, You cannot use local drive .");
                 }
